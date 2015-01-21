76b44071c801bd05e2fcfae5b00b4728489e6786
David Schor <davidschor@zigwap.com>
2015-01-21 00:47:48 -0500

    unicode info / set functions

---
+++ src/set.c
@@ -0,0 +1,42 @@
+a_str a_set(a_str str, a_str newstr) 
+{
+    size_t size;
+    assert(str != NULL && newstr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && newstr != NULL, NULL);
+    
+    size = a_size(newstr);
+    str = a_reserve(str, size);
+    
+    if (str)
+    {
+        memcpy(a_header(str), a_header(newstr), sizeof (struct a_header) + size);
+        str[size] = '\0';
+    }
+    return str;
+}
+
+a_str a_set_cstr(a_str str, const char *newstr) 
+{
+    assert(str != NULL && newstr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && newstr != NULL, NULL);
+    A_ASSERT_UTF8(newstr);
+    return a_set_cstr_size(str, newstr, strlen(newstr));
+}
+
+a_str a_set_cstr_size(a_str str, const char *newstr, size_t size) 
+{
+    assert(str != NULL && newstr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && newstr != NULL, NULL);
+    A_ASSERT_UTF8(newstr);
+    str = a_reserve(str, size);
+    
+    if (str)
+    {
+        struct a_header *h = a_header(str);
+        memcpy(str, newstr, size);
+        str[size] = '\0';
+        h->len = a_len_cstr_max(str, size);
+        h->size = size;
+    }
+    return str;
+}
---
+++ src/unicode.c
@@ -0,0 +1,13 @@
+const char *a_unicode_version() 
+{
+    return "7.0.0";
+}
+const char *a_unicode_version_reference() 
+{
+    return "The Unicode Consortium. The Unicode Standard, Version 7.0.0, (Mountain View, CA: The Unicode Consortium, 2014. ISBN 978-1-936213-09-2)";
+}
+const char *a_unicode_version_url()
+{
+    return "http://www.unicode.org/versions/Unicode7.0.0/";
+}
+
