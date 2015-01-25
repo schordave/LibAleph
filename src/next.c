8b7f250410fa08ba1c26271286e14b9d7435f49e
David Schor <davidschor@zigwap.com>
2015-01-25 01:20:49 -0500

    next

---
+++ src/next.c
@@ -0,0 +1,101 @@
+const char a_next_char_size[256] = 
+{
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 
+    2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
+    4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1
+};
+
+
+/*
+ * Copyright (c) 2006-2015 David Schor (david@zigwap.com), ZigWap LLC
+ * 
+ * License: MIT
+ */
+char *a_next(const char **s)
+{
+    assert(s != NULL);
+    PASSTHROUGH_ON_FAIL(s != NULL, NULL);
+    return (char*)(*s = *s + a_size_chr_cstr(*s));
+}
+
+a_cp a_next_cp(const char **s)
+{
+    const unsigned char *p = (const unsigned char*)*s;
+    assert(s != NULL);
+    PASSTHROUGH_ON_FAIL(s != NULL, 0);
+    
+    switch (a_size_chr_cstr(p))
+    {
+        case 1:
+            *s += 1;
+            return *p;
+        case 2:
+            *s += 2;
+            return ((*p & 0x1F) << 6) | (p[1] & 0x3F);
+        case 3:
+            *s += 3;
+            return ((*p & 0x0F) << 12) | ((p[1] & 0x3F) << 6) | (p[2] & 0x3F);
+        case 4:
+            *s += 4;
+            return ((*p & 0x07) << 18) | ((p[1] & 0x3F) << 12) | ((p[2] & 0x3F) << 6) | (p[3] & 0x3F);
+        default:
+            return -1;
+    }
+#if 0
+    unsigned char c = *p;
+    
+    
+    if (c < 0x80)
+    {
+        *s += 1;
+        return c;
+    }
+    if ((c & 0xE0) == 0xC0/*&& (p[1] & 0xC0) == 0x80*/)
+    {
+        *s += 2;
+        return ((c & 0x1F) << 6) | (p[1] & 0x3F);
+    }
+    if ((c & 0xF0) == 0xE0/* && (p[1] & 0xC0) == 0x80 && (p[2] & 0xC0) == 0x80*/)
+    {
+        *s += 3;
+        return ((c & 0x0F) << 12) | ((p[1] & 0x3F) << 6) | (p[2] & 0x3F);
+    }
+    if ((c & 0xF8) == 0xF0/* && (p[1] & 0xC0) == 0x80 && (p[2] & 0xC0) == 0x80 && (p[3] & 0xC0) == 0x80*/)
+    {
+        *s += 4;
+        return ((c & 0x07) << 18) | ((p[1] & 0x3F) << 12) | ((p[2] & 0x3F) << 6) | (p[3] & 0x3F);
+    }
+    return -1;
+#endif
+}
+
+a_cp a_peek(const char *s)
+{
+    const unsigned char *p = (const unsigned char*)s;
+    assert(s != NULL);
+    PASSTHROUGH_ON_FAIL(s != NULL, 0);
+    
+    switch (a_size_chr_cstr(s))
+    {
+        case 1:
+            return *p;
+        case 2:
+            return ((*p & 0x1F) << 6) | (p[1] & 0x3F);
+        case 3:
+            return ((*p & 0x0F) << 12) | ((p[1] & 0x3F) << 6) | (p[2] & 0x3F);
+        case 4:
+            return ((*p & 0x07) << 18) | ((p[1] & 0x3F) << 12) | ((p[2] & 0x3F) << 6) | (p[3] & 0x3F);
+        default:
+            return -1;
+    }
+}
