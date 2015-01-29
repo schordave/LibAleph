71b47ecb550668de45290bdb5274242a89f7da1d
David Schor <davidschor@zigwap.com>
2015-01-28 23:09:20 -0500

    endswith

--- src/cmp.c
+++ src/cmp.c
@@ -163,3 +163,51 @@ int a_icmpn_cstr_cstr(const char *str1, const char *str2, size_t n)
     
     return -1;
 }
+int a_ircmp_cstr_cstr(const char *str1, const char *str2, size_t len1, size_t len2)
+{
+    size_t index_a, index_b;
+    size_t count_a, count_b;
+    a_cp buff_a[A_MAX_CASEFOLD_SIZE+1];/*  */
+    a_cp buff_b[A_MAX_CASEFOLD_SIZE+1];
+    
+    index_a = index_b = 0;
+    count_a = count_b = 0;
+    buff_a[0] = buff_b[0] = 0;
+    
+    str1 += len1;
+    str2 += len2;
+    for (;;)
+    {
+        if (count_a >= len1 || count_b >= len2) /* if we got passed N, they were equal */
+            return 0;
+        
+        /* if the buffer is empty, grab the next codepoint and casefold it */
+        if (!buff_a[index_a])
+        {
+            a_to_fold_cp_cp(a_prev_cp(&str1), buff_a);
+            index_a = 0;
+            ++count_a;
+        }
+        if (!buff_b[index_b])
+        {
+            a_to_fold_cp_cp(a_prev_cp(&str2), buff_b);
+            index_b = 0;
+            ++count_b;
+        }
+        
+        /* exit point - if one of the strings NULL terminated */
+        if (!buff_a[index_a] || !buff_b[index_b])
+            return (buff_a[index_a] == buff_b[index_b]) ? 0 : -1;
+        
+        /* compare the buffers until one of them is empty
+         * jump to the start and re-fill the buffer */
+        while (buff_a[index_a] && buff_b[index_b])
+        {
+            if (buff_a[index_a] != buff_b[index_b])
+                return -1;
+            index_a++, index_b++;
+        }
+    }
+    
+    return -1;
+}
--- src/startswith.c
+++ src/startswith.c
@@ -3,19 +3,20 @@
  * 
  * License: MIT
  */
+
 int a_startswith(a_cstr str, a_cstr substr)
 {
     assert(str != NULL && substr != NULL);
     PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
     A_ASSERT_UTF8(substr);
-    return (strncmp(str, substr, a_size(substr)) == 0);
+    return (memcmp(str, substr, a_size(substr)) == 0);
 }
 int a_startswith_cstr(a_cstr str, const char *substr)
 {
     assert(str != NULL && substr != NULL);
     PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
     A_ASSERT_UTF8(substr);
-    return (strncmp(str, substr, strlen(substr)) == 0);
+    return (memcmp(str, substr, strlen(substr)) == 0);
 }
 int a_istartswith(a_cstr str, a_cstr substr)
 {
@@ -29,4 +30,50 @@ int a_istartswith_cstr(a_cstr str, const char *substr)
     PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
     A_ASSERT_UTF8(substr);
     return (a_icmp_min_cstr_cstr(str, substr) == 0);
+}
+
+/**************************************************************/
+/**************************************************************/
+
+static int a_endswith_internal(const char *str, const char *substr, size_t size_a, size_t size_b)
+{
+    if (size_b > size_a)
+        return 0;
+    return (memcmp(str + size_a - size_b, substr, size_b) == 0);
+}
+int a_endswith(a_cstr str, a_str substr)
+{
+    assert(str != NULL && substr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
+    return a_endswith_internal(str, substr, a_size(str), a_size(substr));
+}
+int a_endswith_cstr(a_cstr str, const char *substr)
+{
+    assert(str != NULL && substr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
+    return a_endswith_internal(str, substr, a_size(str), strlen(substr));
+}
+int a_endswith_cstr_cstr(const char *str, const char *substr)
+{
+    assert(str != NULL && substr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
+    return a_endswith_internal(str, substr, strlen(str), strlen(substr));
+}
+int a_iendswith(a_cstr str, a_str substr)
+{
+    assert(str != NULL && substr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
+    return (a_ircmp_cstr_cstr(str, substr, a_size(str), a_size(substr)) == 0);
+}
+int a_iendswith_cstr(a_cstr str, const char *substr)
+{
+    assert(str != NULL && substr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
+    return (a_ircmp_cstr_cstr(str, substr, a_size(str), strlen(substr)) == 0);
+}
+int a_iendswith_cstr_cstr(const char *str, const char *substr)
+{
+    assert(str != NULL && substr != NULL);
+    PASSTHROUGH_ON_FAIL(str != NULL && substr != NULL, 0);
+    return (a_ircmp_cstr_cstr(str, substr, strlen(str), strlen(substr)) == 0);
 }
