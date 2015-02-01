713dbfe02adc1c34ab9b6ed14d258db245c6b7c4
David Schor <davidschor@zigwap.com>
2015-02-01 04:56:47 -0500

    fixed empty

--- verif/26.unicode_version.c
+++ verif/26.unicode_version.c
@@ -1,1 +1,34 @@
- 
+/*
+ * Copyright (c) 2006-2015 David Schor (davidschor@zigwap.com), ZigWap LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ctest.h"
+#include "aleph.h"
+
+CTEST(Version, check_version)
+{ 
+    ASSERT_EQUAL(0, strcmp(a_unicode_version(), "7.0.0"));
+    ASSERT_EQUAL(0, strcmp(a_unicode_version_reference(), "The Unicode Consortium. The Unicode Standard, Version 7.0.0, (Mountain View, CA: The Unicode Consortium, 2014. ISBN 978-1-936213-09-2)"));
+    ASSERT_EQUAL(0, strcmp(a_unicode_version_url(), "http://www.unicode.org/versions/Unicode7.0.0/"));
+}
+
