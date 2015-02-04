8bbdc89b3c6bda969b93fb4f935c58676bfc6da7
David Schor <davidschor@zigwap.com>
2015-02-04 10:11:58 -0500

    offset calculation

--- src/a_internal.c
+++ src/a_internal.c
@@ -10,3 +10,25 @@ static a_cp     a_internal_char_to_cp(const char *s);
 static a_cp     a_internal_to_next_cp(const char **s);
 static char    *a_internal_to_prev(const char **s);
 static a_cp     a_internal_to_prev_cp(const char **s);
+static size_t   a_internal_index_to_offset(const char *s, size_t index);
+static size_t   a_internal_gindex_to_offset(const char *s, size_t index);
+
+
+static size_t a_internal_index_to_offset(const char *s, size_t index)
+{
+    const char *start;
+    
+    for (start = s; *s && index; --index)
+        a_next_cstr(&s);
+    
+    return (s-start);
+}
+static size_t a_internal_gindex_to_offset(const char *s, size_t index)
+{
+    const char *start;
+    
+    for (start = s; *s && index; --index)
+        a_gnext_cstr(&s);
+    
+    return (s-start);
+}
