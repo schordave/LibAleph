e3c8d529059c5fae747b768f4d659743c14dd756
David Schor <davidschor@zigwap.com>
2015-02-04 03:14:24 -0500

    moved funcs back

--- src/to.c
+++ src/to.c
@@ -3,24 +3,85 @@
  * 
  * License: MIT
  */
+static a_cp a_internal_char_to_cp(const char *s)
+{
+#if 1
+    const unsigned char *p = (const unsigned char*)s;
+    unsigned char c = *p;
+    switch (a_next_char_size[c])
+    {
+        case 1:
+            return *p;
+        case 2:
+            return ((*p & 0x1F) << 6) | (p[1] & 0x3F);
+        case 3:
+            return ((*p & 0x0F) << 12) | ((p[1] & 0x3F) << 6) | (p[2] & 0x3F);
+        case 4:
+            return ((*p & 0x07) << 18) | ((p[1] & 0x3F) << 12) | ((p[2] & 0x3F) << 6) | (p[3] & 0x3F);
+        default:
+            return 0;
+    }
+#elif 0
+    unsigned char c = *s;
+    
+    if (c < 0x80)
+        return c;
+    if ((c & 0xE0) == 0xC0/*&& (s[1] & 0xC0) == 0x80*/)
+        return ((c & 0x1F) << 6) | (s[1] & 0x3F);
+    if ((c & 0xF0) == 0xE0/* && (s[1] & 0xC0) == 0x80 && (s[2] & 0xC0) == 0x80*/)
+        return ((c & 0x0F) << 12) | ((s[1] & 0x3F) << 6) | (s[2] & 0x3F);
+    if ((c & 0xF8) == 0xF0/* && (s[1] & 0xC0) == 0x80 && (s[2] & 0xC0) == 0x80 && (s[3] & 0xC0) == 0x80*/)
+        return ((c & 0x07) << 18) | ((s[1] & 0x3F) << 12) | ((s[2] & 0x3F) << 6) | (s[3] & 0x3F);
+    return -1;
+#endif
+}
 a_cp a_to_cp(const char *s)
 {
     assert(s != NULL);
     PASSTHROUGH_ON_FAIL(s != NULL, 0);
-    return a_internal_critical_char_to_cp(s);
+    return a_internal_char_to_cp(s);
 }
 
+static char *a_internal_cp_to_char(a_cp cp, char *b)
+{
+    unsigned int c = (unsigned int)cp;
+    
+    if (c < 0x80)
+        b[0] = (char)c, b[1] = '\0';
+    else if (c < 0x0800)
+        b[0] = (char)(0xC0 | (c >> 6)), 
+        b[1] = (char)(0x80 | (c & 0x3F)),
+        b[2] = '\0';
+    else if (c < 0x10000)
+        b[0] = (char)(0xE0 | (c >> 12)),
+        b[1] = (char)(0x80 | ((c >> 6) & 0x3F)),
+        b[2] = (char)(0x80 | (c & 0x3F)),
+        b[3] = '\0';
+    else if (c < 0x110000)
+        b[0] = (char)(0xF0 | (c >> 18)),
+        b[1] = (char)(0x80 | ((c >> 12) & 0x3F)),
+        b[2] = (char)(0x80 | ((c >> 6) & 0x3F)),
+        b[3] = (char)(0x80 | (c & 0x3F)),
+        b[4] = '\0';
+    else
+        b[0] = '\0';
+    
+    return b;
+}
 const char *a_to_utf8_size(a_cp cp, char *b, int *s)
 {
+    A_ASSERT_CODEPOINT(cp);
     PASSTHROUGH_ON_FAIL(b != NULL, NULL);
     
-    a_internal_critical_cp_to_char(cp, b);
-    *s = a_size_chr_cstr(b);
+    a_internal_cp_to_char(cp, b);
+    if (s)
+        *s = a_size_chr_cstr(b);
     return b;
 }
 
 const char *a_to_utf8(a_cp c, char *b)
 {
+    A_ASSERT_CODEPOINT(c);
     PASSTHROUGH_ON_FAIL(b != NULL, NULL);
     return a_to_utf8_size(c, b, NULL);
 }
@@ -29,7 +90,7 @@ char *a_to_fold_cp(a_cp cp, char *b)
 {
     const struct a_char_record *r = A_RECORD_PTR(cp);
     register int new_cp = cp + r->casefold_diff;
-    assert(A_MIN_CP <= cp && cp <= A_MAX_CP);
+    A_ASSERT_CODEPOINT(cp);
     assert(b != NULL);
     PASSTHROUGH_ON_FAIL(b != NULL, NULL);
     
@@ -57,7 +118,7 @@ char *a_to_fold_cp(a_cp cp, char *b)
     
     /* common case */
     if (!(r->special & A_SPECIAL_MASK_CASEFOLD))
-        return a_internal_critical_cp_to_char(new_cp, b);
+        return a_internal_cp_to_char(new_cp, b);
     
     /* casefolding has a set of exceptional characters
      * that fold into multiple code points, sometimes
@@ -97,7 +158,7 @@ char *a_to_fold_cp(a_cp cp, char *b)
 a_cp *a_to_fold_cp_cp(a_cp cp, a_cp *b)
 {
     const struct a_char_record *r = A_RECORD_PTR(cp);
-    assert(A_MIN_CP <= cp && cp <= A_MAX_CP);
+    A_ASSERT_CODEPOINT(cp);
     assert(b != NULL);
     PASSTHROUGH_ON_FAIL(b != NULL, NULL);
     
@@ -169,16 +230,16 @@ a_cp *a_to_fold_cp_cp(a_cp cp, a_cp *b)
 
 char *a_to_fold_simple_chr(a_cp cp, char *b)
 {
-    assert(A_MIN_CP <= cp && cp <= A_MAX_CP);
+    A_ASSERT_CODEPOINT(cp);
     assert(b != NULL);
     PASSTHROUGH_ON_FAIL(b != NULL, NULL);
     
-    return a_internal_critical_cp_to_char(
+    return a_internal_cp_to_char(
                 cp + A_RECORD_PTR(cp)->simplecasefold_diff, b);
 }
 
 a_cp a_to_fold_simple_chr_cp(a_cp cp)
 {
-    assert(A_MIN_CP <= cp && cp <= A_MAX_CP);
+    A_ASSERT_CODEPOINT(cp);
     return cp + A_RECORD_PTR(cp)->simplecasefold_diff;
 }
