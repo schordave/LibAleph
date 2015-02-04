e9f61dac2c419a4d9a6d5b92b7c0089aba68b2cf
David Schor <davidschor@zigwap.com>
2015-02-04 10:10:26 -0500

    index-based version

--- src/ins.c
+++ src/ins.c
@@ -4,25 +4,65 @@
  * License: MIT
  */
 
+static a_str a_ins_internal(a_str str, const char * const instr,
+                            const size_t offset, const size_t size, 
+                            const size_t len)
+{
+    struct a_header *h;
+    
+    str = a_ensure(str, size);
+    if (!str)
+        return NULL;
+    
+    h = a_header(str);
+    memmove(str+offset+size, str+offset, h->size - offset + 1);
+    memcpy(str+offset, instr, size);
+    h->size += size;
+    h->len += len;
+    
+    return str;
+}
+
 /*
  * insert string str2 into str at positin index'th.
  */
 a_str a_ins(a_str str, a_cstr str2, size_t index)
 {
-    size_t size;
-    struct a_header *h, *h2;
+    struct a_header *h;
     assert(str != NULL && str2 != NULL);
-    assert(index <= (a_size(str) + a_size(str2)));
+    assert(a_size(str) >= index); /* only treats index as offset */
     
-    h2 = a_header(str2);
-    size = h2->size;
-    str = a_ensure(str, size);
-    h = a_header(str);
+    h = a_header(str2);
+    return a_ins_internal(str, str2, 
+                a_internal_index_to_offset(str, index),
+                h->size, h->len);
+}
+a_str a_ins_chr(a_str str, const char *chr, size_t index)
+{
+    assert(str != NULL && chr != NULL);
+    assert(a_size(str) >= index); /* only treats index as offset */
     
-    memmove(str+index+size, str+index, h->size - index + 1);
-    memcpy(str+index, str2, size);
+    return a_ins_internal(str, chr, 
+                a_internal_index_to_offset(str, index),
+                a_size_chr_cstr(chr), 1);
+}
+a_str a_ins_cstr(a_str str, const char *str2, size_t index)
+{
+    assert(str != NULL && str2 != NULL);
+    assert(a_size(str) >= index); /* only treats index as offset */
     
-    h->size += size;
-    h->len += h2->len;
-    return str;
+    return a_ins_internal(str, str2,
+                a_internal_index_to_offset(str, index),
+                strlen(str2), a_len_cstr(str2));
+}
+a_str a_ins_cp(a_str str, a_cp cp, size_t index)
+{
+    int s;
+    char chr[A_MAX_CHAR];
+    assert(str != NULL);
+    A_ASSERT_CODEPOINT(cp);
+    
+    a_to_utf8_size(cp, chr, &s);
+    return a_ins_internal(str, chr,
+                a_internal_index_to_offset(str, index), s, 1);
 }
