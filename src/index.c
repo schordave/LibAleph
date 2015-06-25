23f0eb5d7aee4edf7ef96c1f7a42faba079fc0fb
David Schor <davidschor@zigwap.com>
2015-06-25 06:39:47 -0400

    merged from char

--- src/index.c
+++ src/index.c
@@ -5,8 +5,15 @@
  */
 
 /*
- * Returns offset from index
+ * Index
  */
+
+a_cp a_char_at(a_cstr str, size_t index)
+{
+    assert("Function not implemented.");
+    return 0;
+}
+/* Returns offset from index */
 size_t a_char_offset(a_cstr str, size_t index)
 {
     return a_char_offset_cstr(str, index);
@@ -18,7 +25,6 @@ size_t a_char_offset_cstr(const char *str, size_t index)
 
     return a_internal_index_to_offset(str, index);
 }
-
 size_t a_char_index(a_cstr str, size_t offset)
 {
     return a_char_index_cstr(str, offset);
@@ -33,15 +39,31 @@ size_t a_char_index_cstr(const char *str, size_t offset)
     
     return index;
 }
-
-
-/*
- * Returns offset from index from reverse
- */
+/* Returns offset from index from reverse */
 size_t a_char_offset_rev(a_cstr str, size_t index)
 {
     assert(str != NULL && index <= a_len(str));
     PASSTHROUGH_ON_FAIL(str != NULL, 0);
     
     return a_internal_index_to_offset_rev(str, index);
+}
+size_t a_char_offset_rev_cstr(const char *str, size_t index)
+{
+    assert("Function not implemented.");
+    return 0;
+}
+size_t a_char_index_rev(a_cstr str, size_t offset)
+{
+    assert("Function not implemented.");
+    return 0;
+}
+size_t a_char_index_rev_cstr(const char *str, size_t offset)
+{
+    assert("Function not implemented.");
+    return 0;
+}
+char *a_gchar_at(a_str str, size_t index)
+{
+    assert("Function not implemented.");
+    return 0;
 }
